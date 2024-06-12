9e428ca4e74e538407da9cafd731407cdcf1aec8
Luciano <lulopeslulopes@yahoo.com>
2024-06-11 22:48:41 -0300

    Código para um processo seletivo

---
+++ src/Contar.java
@@ -0,0 +1,19 @@
+public class Contar{
+    public static void contar(int parametroUm, int parametroDois) {
+        int diferenca = parametroDois - parametroUm;
+
+        for(diferenca = 0;diferenca < parametroDois; diferenca++){
+            
+            System.out.println("Imprimindo o numero: " + diferenca);
+            
+            
+        }
+            return;
+        }
+        
+        
+    }
+
+
+
+//diferenca = 0; parametroUm < parametroDois;diferenca++
---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException  extends Exception{
+    public void InvalidException(int parametroUm, int parametroDois){
+        
+        System.out.println("O primeiro numero não pode ser maior que o segundo!");
+    }
+}
